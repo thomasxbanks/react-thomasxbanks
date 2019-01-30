cd3ce5b322bab7db348cfe27bcc407f76bb00c8a
Thomas Banks <thomas.banks@havaslynx.com>
2019-01-30 11:43:58 +0000

    Updates

---
+++ precache-manifest.0b18235dc3159bb41b910f5a2873ba9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "71f2527d1be23857d944",
+    "url": "/react-thomasxbanks/static/js/runtime~main.71f2527d.js"
+  },
+  {
+    "revision": "4e1a5fb9ea84e8486030",
+    "url": "/react-thomasxbanks/static/js/main.4e1a5fb9.chunk.js"
+  },
+  {
+    "revision": "61556b548c1ed7de40e3",
+    "url": "/react-thomasxbanks/static/js/1.61556b54.chunk.js"
+  },
+  {
+    "revision": "4e1a5fb9ea84e8486030",
+    "url": "/react-thomasxbanks/static/css/main.ee9dec39.chunk.css"
+  },
+  {
+    "revision": "8e51869395cc5bc59a9ae56832f3e0d5",
+    "url": "/react-thomasxbanks/index.html"
+  }
+];
